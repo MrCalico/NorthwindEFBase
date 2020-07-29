a73e9684a3ef415a09f6a9fabd1b4a5fe01cc689
John Kallie <JKallie@ApexMicro.com>
2020-07-29 10:18:36 -0700

    Add project files.

---
+++ NorthwindEFBase/Data/CustomersRepository.cs
@@ -0,0 +1,25 @@
+﻿using NorthwindEFBase.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindEFBase.Data
+{
+    public class CustomersRepository : SqlRepository<Customer>
+    {
+        public CustomersRepository(NorthwindDbContext context)
+            : base(context)
+        {
+        }
+
+        public override IQueryable<Customer> GetAll()
+        {
+            return EfDbSet;
+        }
+
+        public override async Task<Customer> GetById(object id)
+        {
+            return await GetAll().SingleOrDefaultAsync(c => c.CustomerID == (string)id);
+        }
+    }
+}
---
+++ NorthwindEFBase/Data/EmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using NorthwindEFBase.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NorthwindEFBase.Data
+{
+    public class EmployeeRepository : SqlRepository<Employee>
+    {
+        public EmployeeRepository(NorthwindDbContext context)
+            : base(context)
+        {
+        }
+
+        public override IQueryable<Employee> GetAll()
+        {
+            return EfDbSet;
+        }
+
+        public override async Task<Employee> GetById(object id)
+        {
+            return await GetAll().SingleOrDefaultAsync(c => c.EmployeeID == (int)id);
+        }
+    }
+}
